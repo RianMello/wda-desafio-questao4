34fe376b58f4bebdfb44ed9d50b688b8378819aa
RianMello <rianfmello05@gmail.com>
2022-02-22 06:28:30 -0400

    altered config

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     method: 'GET',
-    baseURL: 'http://livraria--back.herokuapp.com/'
+    baseURL: 'https://livraria--back.herokuapp.com/'
 })
 
 export default api
