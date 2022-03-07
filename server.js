abb6acc081d92cc6fee7acb8fb90b832f0df747d
RianMello <rianfmello05@gmail.com>
2022-03-07 12:47:19 -0400

    fix: adicionado rotas estaticas

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const { resolve } = require('path')
 const app = express()
 
 app.use('/', express.static(resolve(__dirname, './build')))
-app.use('/**', express.static(resolve(__dirname, './build')))
+app.use('/books', express.static(resolve(__dirname, './build')))
+app.use('/publishers', express.static(resolve(__dirname, './build')))
+app.use('/users', express.static(resolve(__dirname, './build')))
+app.use('/rents', express.static(resolve(__dirname, './build')))
 
 app
 .listen(process.env.PORT || 3000, (err) => {
